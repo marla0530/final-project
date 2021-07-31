1371ad25ef8ece0cbdc69d6c70b0203bb5466f7c
MintDeer <nicoperez01@gmail.com>
2021-07-31 15:41:05 -0500

    Add files via upload
    Max heap implementation

---
+++ Project3/maxHeap.h
@@ -0,0 +1,30 @@
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <ctime>
+#include <chrono>
+#include <iomanip>
+
+using namespace std;
+
+class maxHeap {
+    private:
+        vector<pair<string,double>> heap;
+
+    public:
+        maxHeap();
+
+        void insert(string name, double power);
+        void pop();
+        pair<string, double> checkTop();
+
+        bool insertCSV(string filename);
+
+        vector<pair<string, double >> topX(int x);
+
+
+
+};
+
+
