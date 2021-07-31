be2515cdd6a26b2ca9409be74e9b73826be3baa1
marla0530 <65103069+marla0530@users.noreply.github.com>
2021-07-31 17:20:15 -0400

    Update vecVector.h

--- Project3/vecVector.h
+++ Project3/vecVector.h
@@ -13,39 +13,3 @@ public:
 	void top25();
 	vecVector();
 };
-vecVector::vecVector() {
-	for (int i = 0; i < 10001; i++) {
-		vector <string> temp;
-		table.push_back(temp);
-	}
-}
-int vecVector::hashFunction(double& key) {
-	return key * 100;
-}
-void vecVector::insert(double& key, string& teamName) {
-	int index = hashFunction(key);
-	if (index > largest) {
-		largest = index;
-	}
-	table[index].push_back(teamName);
-}
-void vecVector::top25() {
-	int count = 0;
-	int temp = largest;
-	while (count < 25) {
-		for (int i = 0; i < table[temp].size();i++) {
-			cout << count + 1 << ", " << table[temp][i] << endl;
-			count++;
-			if (count == 25) {
-				break;
-			}
-		}
-		for (int i = temp - 1; temp > -1; i--)
-		{
-			if (table[i].size() != 0) {
-				temp = i;
-				break;
-			}
-		}
-	}
-}
