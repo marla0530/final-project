af4c5bc685a62958ed5ba0db7c4b7b1ebae16cf0
marla0530 <65103069+marla0530@users.noreply.github.com>
2021-08-01 00:05:12 -0400

    Update bst.cpp

--- Project3/bst.cpp
+++ Project3/bst.cpp
@@ -6,59 +6,66 @@ node* BST::insert(node* root, double& key, string& teamName) {
     if (root == NULL) {
         return new node(key, teamName);
     }
-    
+
     //if node already exist within the tree push it into the vector on the nodes
     else if (root->powerlevel == key) {
         root->teams.push_back(teamName);
         return root;
     }
-    
+
+
     //traverse left
     if (key < root->powerlevel) {
         root->left = insert(root->left, key, teamName);
     }
+
     
     //traverse right
     else {
         root->right = insert(root->right, key, teamName);
 
     }
+
     return root;
 }
 
+
 //return the kth largest number in the tree
-void BST::kLargest(node* root, int& k) {
+pair<string, double> BST::kLargest(node* root, int& k) {
     if (root == NULL) {
         return;
     }
-    
+
     //reverse inorder traversal
     kLargest(root->right, k);
-    
+ 
     //when k = 0 we have found the targeted kth largest
     if (k <= 0) {
         return;
     }
-    
-    //if kth largest is within the vector print it out and set k to 0 
+
+    //if kth largest is within the vector return it and set k to 0 
     else if (k <= root->teams.size()) {
-        cout << root->teams[k - 1] << endl;
         k = 0;
-        return;
+
+        return make_pair(root->teams[k - 1],root->powerlevel);
     }
-    
+
     //decrease k by the number of teams in the vector
     else {
+
         k -= root->teams.size();
     }
+
     kLargest(root->left, k);
 }
 
-//call kLargest 25 times
-void BST::top25() {
-    for (int i = 1; i < 26; i++) {
-        cout << i << ", ";
-        int temp = i;
-        kLargest(root, temp);
+//call kLargest x times
+vector<pair<string, double >> BST::topX(int x) {
+    
+    vector<pair<string, double >> temp;
+    for (int i = 1; i < x; i++) {
+                
+        temp.push_back(kLargest(root, x));
     }
 }
